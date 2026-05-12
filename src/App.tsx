3f5c3bca459e01bd0552faced1fa6686c29b0cd3
mostafa3122 <moustafahelal92@gmail.com>
2026-05-12 03:22:50 +0300

    refactor: update folder structure and add auth apis and interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Header from "./shared/Header/Header";
 
 function App() {
   return (
     <>
       <h1 className="bg-amber-300 py-5 text-center ">PMS test</h1>
+      <Header/>
       
     </>
   );
--- src/services/LoginApi.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function LoginApi() {
-  return <div>LoginApi</div>;
-}
